13eff1b4e4a66a3f4babf7021b30a21aef495e70
David Rocha <icept96@gmail.com>
2026-03-04 18:42:04 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+/* DavElec minimal SW (v620) */
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
